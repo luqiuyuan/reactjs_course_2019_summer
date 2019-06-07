a7447f7702c345e6e57c1d0e5173325445fd43f1
luqiuyuan <lsun100@uottawa.ca>
2019-06-07 14:14:06 -0400

    Add lecture00

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button, { Title, Footer } from './components/Button';
 import './App.css';
 
 function App() {
+  const showAlert = () => {
+    alert("Hello ReactJS!");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Panel">
+        <Button buttonFunction={showAlert} label="Sign Up" />
+      </div>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import styles from './styles/Button';
+
+export default class Button extends Component {
+
+    state = {
+        hovered: false,
+    };
+
+    render() {
+        return (
+            <div
+                style={{...styles.container, opacity: this.state.hovered? 0.5 : 1}}
+                onMouseEnter={this.startHover}
+                onMouseLeave={this.endHover}
+                onClick={this.props.buttonFunction}>
+                <p style={styles.label}>{this.props.label}</p>
+            </div>
+        );
+    }
+
+    startHover = () => {
+        this.setState({ hovered: true });
+    }
+
+    endHover = () => {
+        this.setState({ hovered: false });
+    }
+
+}
