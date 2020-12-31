4535e5cef115d65408024adedfe7e92807e11311
luqiuyuan <lsun100@uottawa.ca>
2020-12-30 19:58:30 -0500

    upgrade react version

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import Button, { Title, Footer } from './components/Button';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const showAlert = () => {
-    alert("Hello ReactJS!");
-  }
-
   return (
     <div className="App">
-      <div className="Panel">
-        <Button buttonFunction={showAlert} label="Sign Up" />
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
